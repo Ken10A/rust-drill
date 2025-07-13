e23c883c8fbe293e00d21d9f20c2fd93db64c619
Ken10A <cent.rngttwt@gmail.com>
2025-07-13 23:29:39 +0900

    add: use clap argument parser

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,10 @@
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Ken10A <sample@example.com>")
+        .about("Rust Echo")
+        .get_matches();
+    println!("{:?}", std::env::args());
 }
