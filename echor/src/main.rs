c084e65bb5168675f2980c56f121725e1e33bf1d
Ken10A <cent.rngttwt@gmail.com>
2025-11-24 17:22:18 +0900

    add: omit newline and string args

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,10 +1,24 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Ken10A <sample@example.com>")
         .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .long("omit-newline")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
         .get_matches();
-    println!("{:?}", std::env::args());
+    println!("{:#?}", matches);
 }
