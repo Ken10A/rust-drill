806c570b776c25f61950af57a0f99b4bd9d8e451
Ken10A <cent.rngttwt@gmail.com>
2025-11-29 18:24:55 +0900

    add: lib file

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,28 +1,6 @@
-use clap::{App, Arg};
-
 fn main() {
-    let matches = App::new("echor")
-        .version("0.1.0")
-        .author("Ken10A <sample@example.com>")
-        .about("Rust Echo")
-        .arg(
-            Arg::with_name("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .min_values(1),
-        )
-        .arg(
-            Arg::with_name("omit_newline")
-                .short("n")
-                .long("omit-newline")
-                .help("Do not print newline")
-                .takes_value(false),
-        )
-        .get_matches();
-
-    let text = matches.values_of_lossy("text").unwrap();
-    let omit_newline = matches.is_present("omit_newline");
-
-    println!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+    if let Err(e) = echor::run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
